dadf411c04a9c9fdac7b0f569e68fa6ddaad1605
Vaishnavi Gupta <54323054+vaishnavig20@users.noreply.github.com>
2020-05-14 17:37:54 +0530

    Update drum.js

--- drum.js
+++ drum.js
@@ -3,13 +3,12 @@ const audio =document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key =document.querySelector(`.key[data-key="${e.keyCode}"]`);
    if(!audio) return;
     audio.currentTime=0;
-    audio.play()
-    console.log(key);
+    audio.play();
     key.classList.add('playing');
 }
 function removeTransition(e){
     if(e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
+    this.classList.remove('playing');
 }
 const keys=Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend',removeTransition));
